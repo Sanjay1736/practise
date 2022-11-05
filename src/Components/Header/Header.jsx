16d375383aa0d96673ef6c4cf4f127c43a095776
Sanjay1736 <92837525+Sanjay1736@users.noreply.github.com>
2022-11-05 19:21:34 +0530

    Components: Created New Header Folder and Move old Folders Into Header folder and Created New files For Login & Search Elements

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,14 @@
-import "./Header.module.css";
+import style from "./Header.module.css";
+import { Login } from "./Login";
+import { SearchBar } from "./SearchBar";
 export function Header() {
   return (
-    <div>
-      <header className="head">
-        <h1 className="text">Eagle</h1>
+    <div className={style.head}>
+      <header>
+        <h1 className={style.text}>Eagle</h1>
       </header>
+      <Login />
+      <SearchBar />
     </div>
   );
 }
---
+++ src/Components/Header/SearchBar.jsx
@@ -0,0 +1,8 @@
+export function SearchBar() {
+  return (
+    <div>
+      <input type="text" placeholder="Search Here" />
+      <button type="button">Search</button>
+    </div>
+  );
+}
